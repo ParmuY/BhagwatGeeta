8b031e8edd94b3358e670e19e6e8dee512f3f2b2
ParmuY <pramesh2151@gmail.com>
2021-03-18 04:43:51 -0700

    commit

--- app/src/main/java/com/parmu/bhagwatgeeta/activities/Adhyay1_AC1.java
+++ app/src/main/java/com/parmu/bhagwatgeeta/activities/Adhyay1_AC1.java
@@ -7,7 +7,7 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
+
 
 import com.google.android.material.tabs.TabLayout;
 import com.parmu.bhagwatgeeta.R;
