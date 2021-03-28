5cb904488c4f443f18e4565732fae5ac764c0fd3
ParmuY <pramesh2151@gmail.com>
2021-03-28 03:30:02 -0700

    separate textview in chapter17,16,15

--- app/src/main/java/com/parmu/bhagwatgeeta/fragments/Fragment15.java
+++ app/src/main/java/com/parmu/bhagwatgeeta/fragments/Fragment15.java
@@ -72,8 +72,8 @@ public class Fragment15 extends Fragment{
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view15= inflater.inflate(R.layout.fragment_15, container, false);
-        tvSanskrit =view15.findViewById(R.id.sanskrit_16);
-        tvBhavarth = view15.findViewById(R.id.bhavarth_16);
+        tvSanskrit =view15.findViewById(R.id.sanskrit_15);
+        tvBhavarth = view15.findViewById(R.id.bhavarth_15);
         tvSanskrit.setText(getArguments().getString("sanskrit15"));
         tvBhavarth.setText(getArguments().getString("bhavarth15"));
         constraintLayout =view15.findViewById(R.id.constrained_layout);
