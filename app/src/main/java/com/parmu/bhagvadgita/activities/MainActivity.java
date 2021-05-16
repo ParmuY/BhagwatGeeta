ea1018bd5ecd25fb7addab4bf156f1e5d5ef6bb7
ParmuY <pramesh2151@gmail.com>
2021-05-16 03:51:52 -0700

    optimization

--- app/src/main/java/com/parmu/bhagvadgita/activities/MainActivity.java
+++ app/src/main/java/com/parmu/bhagvadgita/activities/MainActivity.java
@@ -27,7 +27,7 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
-
+//code optimization and improvization
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     @SuppressLint("UseSwitchCompatOrMaterialCode")
